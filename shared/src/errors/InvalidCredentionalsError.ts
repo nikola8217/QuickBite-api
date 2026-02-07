be3b623259d71c06882be0314d2dba7cc6952787
nikola8217 <gileer404@gmail.com>
2026-02-07 16:01:57 +0100

    Updates

--- shared/src/errors/InvalidCredentionalsError.ts
+++
@@ -1,7 +0,0 @@
-import { AppError } from "./AppError";
-
-export class InvalidCredentionalsError extends AppError {
-    constructor(message = "Invalid credentionals") {
-        super(message, 401); 
-    }
-}
