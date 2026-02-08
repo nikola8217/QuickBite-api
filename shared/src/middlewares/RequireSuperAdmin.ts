4918b03d6e25c2aea734d91c1d21d8899fc09743
nikola8217 <gileer404@gmail.com>
2026-02-08 19:35:14 +0100

    Updates

--- shared/src/middlewares/RequireSuperAdmin.ts
+++ shared/src/middlewares/RequireSuperAdmin.ts
@@ -6,6 +6,8 @@ export const requireSuperAdmin = (
     res: Response,
     next: NextFunction
 ) => {
+    console.log(123, req.method, req.user);
+
     if (req.user?.role !== "SuperAdmin") {
         throw new NotAuthorizedError();
     }
