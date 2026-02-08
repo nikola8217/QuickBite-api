55720cb419b7a02ef44a7cd533073883766447ea
nikola8217 <gileer404@gmail.com>
2026-02-08 20:04:50 +0100

    Updates

--- shared/src/middlewares/RequireAuth.ts
+++ shared/src/middlewares/RequireAuth.ts
@@ -5,24 +5,25 @@ import { UserPayload } from "../types/express";
 
 export const requireAuth = (jwtSecret: string) => {
 
-  return (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
+    return (req: Request, res: Response, next: NextFunction) => {
+        const authHeader = req.headers.authorization;
 
-    if (!authHeader?.startsWith("Bearer ")) {
-      return next(new NotAuthenticatedError());
-    }
+        if (!authHeader?.startsWith("Bearer ")) {
+        return next(new NotAuthenticatedError());
+        }
 
-    const token = authHeader.split(" ")[1];
+        const token = authHeader.split(" ")[1];
 
-    try {
-      const payload = jwt.verify(token, jwtSecret) as UserPayload;
+        try {
+            const payload = jwt.verify(token, jwtSecret) as UserPayload;
 
-      req.user = payload;
+            req.user = payload;
 
-      next();
-    } catch {
-      return next(new NotAuthenticatedError());
-    }
-  };
-  
+            next();
+        } catch (err) {
+            console.log(err);
+            return next(new NotAuthenticatedError());
+        }
+    };
+
 };
